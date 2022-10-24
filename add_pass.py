ce7d7092f6aa6a864d3feff25d27a03f65e14380
RebelliousInnovation <72558992+RealKien@users.noreply.github.com>
2022-10-23 23:09:45 -0400

    Update add_pass.py

--- add_pass.py
+++ add_pass.py
@@ -3,7 +3,7 @@
 #
 # add_pass = MangagePassword(
 #     context="Password for ADMIN's mail sending account(to send confirmation email)",
-#     password = "bibopyeudau",
+#     password = "xxxxx",
 #     password_id =1)
 #
 # db.session.add(add_pass)
